7b187edf7b89645cf4bfb6f9daadc3fcdc4c1d6a
faxriddindev <faxaxx2022@gmail.com>
2022-04-19 18:40:00 +0500

    header desktop end

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+  
+    extend: {
+      colors:{
+        'main-c': '#100A30',
+        'text-hover-c': '#624DE3'
+      },
+      fontFamily: {
+        'main-f': ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        'h-title': ['55px', '64px'],
+        'm-title': ['48px', '56px']
+      },
   },
-  plugins: [],
+},
+  plugins: []
 }
